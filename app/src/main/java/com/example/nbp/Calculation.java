9ca3b9f1e7d3864dc79381face78302cbad4c788
adann96 <domanski.adam_wojciech@poczta.onet.pl>
2020-12-24 14:25:31 +0100

    Basic spinner added

--- app/src/main/java/com/example/nbp/Calculation.java
+++ app/src/main/java/com/example/nbp/Calculation.java
@@ -3,12 +3,48 @@ package com.example.nbp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Calculation extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Calculation extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String[] country = { "India", "USA", "China", "Japan", "Other"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
+        Spinner spin1 = (Spinner) findViewById(R.id.spinner1);
+        Spinner spin2 = (Spinner) findViewById(R.id.spinner2);
+        spin1.setOnItemSelectedListener(this);
+        spin2.setOnItemSelectedListener(this);
+
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,country);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin1.setAdapter(aa);
+        spin2.setAdapter(aa);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(),country[position] , Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
