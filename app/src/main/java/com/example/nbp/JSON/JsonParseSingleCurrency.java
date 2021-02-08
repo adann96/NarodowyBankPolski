b786927b49e65acee26e9810392fe0372dfd31c3
adann96 <domanski.adam_wojciech@poczta.onet.pl>
2021-02-08 19:07:43 +0100

    Gold calc added

--- app/src/main/java/com/example/nbp/JSON/JsonParseSingleCurrency.java
+++ app/src/main/java/com/example/nbp/JSON/JsonParseSingleCurrency.java
@@ -149,4 +149,57 @@ public class JsonParseSingleCurrency extends AppCompatActivity {
         });
     }
 
+    public static void jsonParsingGoldRanking(String url, ListView listView, Context context, RequestQueue requestQueue) {
+        final ProgressDialog progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    ArrayList<Currency> arrayList = new ArrayList<>();
+                    JSONObject obj = response.getJSONObject(0);
+                    JSONArray jsonArray = obj.getJSONArray("rates");
+                    Currency currency = null;
+
+                    for (int i = 0; i < 35; i++) {
+                        String codeJson = jsonArray.getJSONObject(i).get("code").toString();
+                        String currencyJson = jsonArray.getJSONObject(i).get("currency").toString();
+                        float midJson = Float.parseFloat(jsonArray.getJSONObject(i).get("mid").toString());
+
+                        currency = new Currency(codeJson,currencyJson,midJson);
+                        arrayList.add(currency);
+                    }
+
+                    Collections.sort(arrayList, Comparator.comparing(Currency::getMidJson));
+                    Collections.reverse(arrayList);
+
+                    ArrayAdapter<Currency> arrayAdapter = new ArrayAdapter(context,android.R.layout.simple_list_item_1,arrayList);
+                    listView.setAdapter(arrayAdapter);
+                    progressDialog.dismiss();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                Toast.makeText(context.getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+                Log.d("ERROR uuu", error.getMessage());
+            }
+        });
+        requestQueue.add(request);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(context,"Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/nbp/fragments/Fragment_Gold.java
+++ app/src/main/java/com/example/nbp/fragments/Fragment_Gold.java
@@ -1,20 +1,63 @@
 package com.example.nbp.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.nbp.Calculation;
+import com.example.nbp.EurCurrencyRatesActivity;
+import com.example.nbp.GoldDatesActivity;
 import com.example.nbp.R;
+import com.example.nbp.RankActivity;
+import com.example.nbp.WorldCurrencyRatesActivity;
 
 public class Fragment_Gold extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_gold, container, false);
+
+        CardView goldRate = view.findViewById(R.id.goldRate);
+        CardView datesOfRates = view.findViewById(R.id.datesOfRates);
+        CardView goldCalc = view.findViewById(R.id.goldCalc);
+        CardView aboutApp = view.findViewById(R.id.aboutApp);
+
+        /*
+        goldRate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), EurCurrencyRatesActivity.class);
+                startActivity(intent);
+            }
+        });*/
+        datesOfRates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), GoldDatesActivity.class);
+                startActivity(intent);
+            }
+        });
+        /*
+        goldCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), WorldCurrencyRatesActivity.class);
+                startActivity(intent);
+            }
+        });
+        aboutApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Calculation.class);
+                startActivity(intent);
+            }
+        });*/
 
         return view;
     }
