2585c3ced81e89e56a19bd8b546e1ea4b3a16772
adann96 <domanski.adam_wojciech@poczta.onet.pl>
2021-02-08 19:07:33 +0100

    Gold calc added

---
+++ app/src/main/java/com/example/nbp/GoldDatesActivity.java
@@ -0,0 +1,14 @@
+package com.example.nbp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoldDatesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gold_dates);
+    }
+}
